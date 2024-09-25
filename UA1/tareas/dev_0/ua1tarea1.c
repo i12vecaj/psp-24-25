073028652336f406cb33bbf5b67b5d5b418a71bd
Francisco José Navarro Ruiz <franciscojosenavarro23@gmail.com>
2024-09-25 19:50:06 +0200

    T1
    Tarea 1 - Programación de Procesos en C

--- UA1/tareas/dev_0/ua1tarea1.c
+++ UA1/tareas/dev_0/ua1tarea1.c
@@ -1,1 +1,34 @@
-Ya os gustaría
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+int main()
+{
+  pid_t pid, Padre_pid;
+  int nota;    //Nota es el numero que pediremos despues al usuario
+    printf ("Introduce un numero\n");
+      scanf ("%d", &nota);  //Pedimos el numero con el que trabajaremos mas tarde
+
+  pid = fork();
+  //Designamos ambos procesos y el fork creara ambos y hara de pid el proceso padre
+
+  if (pid == -1)        //Ha ocurrido un error creando el proceso padre
+    {
+      printf ("No se ha podido crear el proceso PADRE");
+      exit (-1);
+    }
+
+      if (pid == 0)           //Nos encontramos en el Proceso padre 
+    {
+      nota-=5;//El proceso padre debe restar 5 y mostrar el resultado
+      printf("Soy el proceso PADRE:\n\t Mi valor habiendo restando 5 es de %d.\n",
+     nota);
+    }
+  else  //Nos encontramos en el proceso hijo
+      {
+      wait (NULL); //espera de la finalizacion del proceso padre
+      nota+=4;            //El proceso hijo suma 4
+      printf("Soy el proceso HIJO \n\t Mi valor habiendo sumado 4 es de %d.\n",
+     nota);
+      }
+  return(0);
+}
