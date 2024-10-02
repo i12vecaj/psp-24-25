fba182996dafe3df089e48c1f2ab2a4f587f14f1
Pakitoh0 <162631375+Pakitoh0@users.noreply.github.com>
2024-10-02 18:20:01 +0200

    ua1tarea1.c

--- UA1/tareas/dev_5/ua1tarea1.c
+++ UA1/tareas/dev_5/ua1tarea1.c
@@ -4,40 +4,38 @@
 #include <sys/wait.h>
 
 int main() {
-  int variable;
-  pid_t pid, Hijo_pid;
-  pid = fork();
+    int variable;
+    pid_t pid, hijo_pid;
 
-printf("introduce un numero");
-  if scanf(  ("%d" variable !=1)){
-    printf("Error en la introducción de datos, introduce un número entero.\n");
-    exit(EXIT_FAILURE);
+    // Solicita un número al usuario
+    printf("Introduce un número: ");
+    
 
+    if (scanf("%d", &variable) != 1) {
+        printf("Error en la introducción de datos, introduce un número entero.\n");
+        exit(EXIT_FAILURE);
+    }
 
-  }
+    // Crea el proceso hijo
+    pid = fork();
 
-  if (pid <0 ) 
-  {
-    printf("No se ha podido crear el proceso hijo...");
-    exit(EXIT_FAILURE);      
-  }
- else if (pid == 0 )  
-  {        
-    printf("Soy el proceso hijo: \n",) 
-    variable += 4;
-    printf("asi que da si le sumamos 4: %d\n " ,variable);
-    exit(EXIT_FAILURE);
-        	
-  }
-  else    
-  { 
-   
-   printf("Soy el proceso padre;:\n");     
-   variable -= 5;
-   printf("asi que da si le restamos 5: %d\n " ,variable);
-    wait(NULL); 
-   printf("Proceso padre después de que el hijo terminó.\n");     
-   }
-   return(0);
+    if (pid < 0) {
+        printf("No se ha podido crear el proceso hijo.\n");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {  // Proceso hijo
+        printf("Soy el proceso hijo.\n");
+        variable += 4;
+        printf("El resultado al sumarle 4 es: %d\n", variable);
+        exit(EXIT_SUCCESS); // Finaliza exitosamente
+    } else {  // Proceso padre
+        printf("Soy el proceso padre.\n");
+        variable -= 5;
+        printf("El resultado al restarle 5 es: %d\n", variable);
+        
+        // Espera a que el proceso hijo termine
+        wait(NULL);
+        printf("Proceso padre después de que el hijo terminó.\n");
+    }
+
+    return 0;
 }
-
