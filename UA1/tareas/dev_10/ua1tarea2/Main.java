a5c678d7f8207fcc14f18f456acb116018262e1f
ignaciodiazjimenez <ignaciodeloyoladiazjimenez@gmail.com>
2024-10-09 16:44:20 +0200

    eliminacion de librerias innecesarias

--- UA1/tareas/dev_10/ua1tarea2/Main.java
+++ UA1/tareas/dev_10/ua1tarea2/Main.java
@@ -1,7 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
     public static void main(String[] args) {
     Thread hilo = new Thread(new Programa1()); 
--- UA1/tareas/dev_10/ua1tarea2/Programa1.java
+++ UA1/tareas/dev_10/ua1tarea2/Programa1.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 //Runnable sirve para poder crear un hilo.
