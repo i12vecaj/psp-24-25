b59db2afeea933ddb41ddcba49b26b9d42c8bcdf
Francisco José Navarro Ruiz <franciscojosenavarro23@gmail.com>
2024-10-09 12:01:56 +0200

    update

--- UA1/tareas/dev_22/UA1tarea2/cadenaCaracteres.java
+++ UA1/tareas/dev_22/UA1tarea2/cadenaCaracteres.java
@@ -1,39 +1,29 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
-public class cadenaCaracteres
-{
-    public static void main(String[] args) {
+public class CadenaCaracteres extends Thread {
 
-        InputStreamReader in = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader (in);
+    public void run() {
+        System.out.println("Inicialización del hilo:");
+        String resultado;
+        resultado = funcadena();
+        System.out.println("El resultado es: " + resultado);
+        System.out.println("Fin del hilo.");
+    }
 
-        try{
-            int salida=0; //Variable para salida de do-while posterior
-            String texto=""; // para salida de do-while
-            String lectura; // string lectura
-
-            do{
-
-                lectura= br.readLine(); //Lee un string entero que está en el buffer,
-
-
-                for(int i=0;i<lectura.length();i++){
-                    //Recorre el string leido y guarda todo hasta un *
-                    if(lectura.charAt(i)=='*'){
-                        salida=1;//Encontrar el '*' sale del do-while
-                        break;
-                    }else{
-                        texto+=lectura.charAt(i);
-                    }
-                }
-
-            }while(salida!=1); //si no encuentra un '*', repetirá el proceso hasta recibir un string que lo contenga
-
-            System.out.println("Total texto almacenado: "+texto);
-            in.close();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+    public String funcadena(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese una cadena de caracteres: ");
+        String cadena = "";
+        boolean validador = false;
+        do {
+            String texto = scanner.nextLine();
+            if (texto.equals("*")) {
+                validador = true;
+            } else {
+                cadena += texto + "\n ";
+            }
+        } while (!validador);
+        scanner.close();
+        return cadena;
     }
 }
