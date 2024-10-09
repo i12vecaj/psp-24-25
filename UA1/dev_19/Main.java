3b3750978762bc3372bc8f1e83a5abdb1b4fd40e
RafaelMora <145365432+Rafael2302@users.noreply.github.com>
2024-10-09 11:27:43 +0200

    Delete UA1/dev_19/Main.java

--- UA1/dev_19/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        // generamos un objeto de la clase Lector para poder llamar a su método
-        Lector lector = new Lector();
-        lector.entradaSalida();//llamamos al método del objeto
-    }
-
-}
