80a8ca73b3ca859ce18f83aa8627edf44850deb7
RafaelMora <145365432+Rafael2302@users.noreply.github.com>
2024-10-09 10:27:08 +0200

    Add files via upload

---
+++ UA1/dev_19/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // generamos un objeto de la clase Lector para poder llamar a su método
+        Lector lector = new Lector();
+        lector.entradaSalida();//llamamos al método del objeto
+    }
+
+}
