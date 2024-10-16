f0550a2d7ce5c93c64be90cdcfe83c1b862b3a92
daniacostac <145365168+daniacostac@users.noreply.github.com>
2024-10-16 18:56:33 +0200

    ua1ex1.c

--- UA1/examen/dev_1/ua1ex1.c
+++ UA1/examen/dev_1/ua1ex1.c
@@ -1,24 +1,16 @@
-/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>  
+#include <unistd.h>  
 
-NOMBRE:
-FECHA:
-
-PARTE PRÁCTICA
-
-FR1: Haz un programa en C que genere una estructura de procesos con un PADRE y 3 HIJOS, del mismo padre se entiende - 2 puntos
-FR2: Visualiza por cada hijo su identificador (si es el hijo 1, 2 ó 3), su PID y el del padre, utilizando para ello una función definida por ti a la que llamen los procesos hijos - 2 puntos
-FR3: Justo antes de finalizar el programa PADRE, se debe imprimir por pantalla el PID del padre de todos una única vez. Debe hacerlo el programa PADRE - 2 puntos
-FR4: Implementa el control de errores - 2 puntos
-FR5: Documenta y estructura el código - 2 puntos
-
-Para evitar complicaciones con máquinas virtuales, si lo prefieres puedes utilizar el compilador online: https://www.onlinegdb.com/online_c_compiler
-
-Notas:
-
-Los comentarios (descriptivos y concisos) en el código ... siempre son bien.
-Los nombres de las variables autodescriptivos ... siempre son bien.
-Las impresión por pantalla, correctamente indentada y verticalmente espaciada ... siempre es bien.
-Los warnings del presente ... son los errores del futuro.
-El nombre del fichero .c a entregar debe ser: examen\dev_X\ua1ex1.c , es decir, el fichero ua1ex1.cdebe estar ubicado en tu carpeta dev_X\
-
-*/
+int main()
+{ 
+//bucle para imprimir los hijos. anteriormente hecho con 3 funciones diferentes, no había caido en esto
+    for ( int i = 0; i < 3; i++ )
+        if ( fork() == 0 )
+        {
+            printf( "ID Hijo: %d del padre: %d\n", getpid(), getppid() );
+            exit(0);
+        }
+    printf( "Padre: %d\n", getpid() ); // pid del proceso padre
+}
