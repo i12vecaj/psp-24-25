f7180d80d38d2061b9f803450aa865a2dc755c7d
Pabloazb <145365889+Pabloazb@users.noreply.github.com>
2024-10-16 18:56:30 +0200

    Add files via upload

--- UA1/examen/dev_0/ua1ex1.c
+++ UA1/examen/dev_0/ua1ex1.c
@@ -1,24 +1,44 @@
-/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-NOMBRE:
-FECHA:
-
-PARTE PRÁCTICA
-
-FR1: Haz un programa en C que genere una estructura de procesos con un PADRE y 3 HIJOS, del mismo padre se entiende - 2 puntos
-FR2: Visualiza por cada hijo su identificador (si es el hijo 1, 2 ó 3), su PID y el del padre, utilizando para ello una función definida por ti a la que llamen los procesos hijos - 2 puntos
-FR3: Justo antes de finalizar el programa PADRE, se debe imprimir por pantalla el PID del padre de todos una única vez. Debe hacerlo el programa PADRE - 2 puntos
-FR4: Implementa el control de errores - 2 puntos
-FR5: Documenta y estructura el código - 2 puntos
-
-Para evitar complicaciones con máquinas virtuales, si lo prefieres puedes utilizar el compilador online: https://www.onlinegdb.com/online_c_compiler
-
-Notas:
-
-Los comentarios (descriptivos y concisos) en el código ... siempre son bien.
-Los nombres de las variables autodescriptivos ... siempre son bien.
-Las impresión por pantalla, correctamente indentada y verticalmente espaciada ... siempre es bien.
-Los warnings del presente ... son los errores del futuro.
-El nombre del fichero .c a entregar debe ser: examen\dev_X\ua1ex1.c , es decir, el fichero ua1ex1.cdebe estar ubicado en tu carpeta dev_X\
-
-*/
+int main()
+{
+    int pid, pid1, pid2;
+  
+    
+    // La variable pid almacena el valor devuelto del fork() system call
+    pid = fork();
+  
+    // Si fork() devuelve 0 indica que es un hijo 
+    if (pid == 0) {
+        printf("child[1] --> pid = %d and ppid = %d\n",
+               getpid(), getppid());
+    }
+  
+    else {
+        pid1 = fork();
+        // segundo hijo.
+        if (pid1 == 0) {
+            printf("child[2] --> pid = %d and ppid = %d\n",
+                   getpid(), getppid());
+        }
+        else {
+            pid2 = fork();
+            if (pid2 == 0) {
+                //tercer hijo.
+                printf("child[3] --> pid = %d and ppid = %d\n",
+                       getpid(), getppid());
+            }
+  
+            //Si el valor retornado por el fork() es >0
+            else {
+                //Este código sólo lo ejecutará el Padre
+                printf("parent --> pid = %d\n", getpid());
+            }
+        }
+    }
+  
+    return 0;
+}
