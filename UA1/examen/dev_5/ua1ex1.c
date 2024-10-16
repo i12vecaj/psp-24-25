55076914bb8c6569da5d9649b38fd22e2f7ed7c0
Pakitoh0 <162631375+Pakitoh0@users.noreply.github.com>
2024-10-16 18:58:52 +0200

    Update ua1ex1.c

--- UA1/examen/dev_5/ua1ex1.c
+++ UA1/examen/dev_5/ua1ex1.c
@@ -1,6 +1,6 @@
 /*
 
-NOMBRE:
+NOMBRE:Paco Barbero
 FECHA:
 
 PARTE PRÁCTICA
@@ -10,6 +10,83 @@ FR2: Visualiza por cada hijo su identificador (si es el hijo 1, 2 ó 3), su PID 
 FR3: Justo antes de finalizar el programa PADRE, se debe imprimir por pantalla el PID del padre de todos una única vez. Debe hacerlo el programa PADRE - 2 puntos
 FR4: Implementa el control de errores - 2 puntos
 FR5: Documenta y estructura el código - 2 puntos
+
+ejercicio :
+
+#include <stdlib.h>
+
+#include <unistd.h>
+
+#include <stdio.h>
+
+void main(){
+    //presentamos el codigo
+      printf("Hola estos son los hijos acompañado de su padre; ");
+    
+    //creamos los procesos hijo
+
+  pid_t pid_hijo1,pid_hijo2,pid_hijo3;
+  
+  
+  //hijo 1
+
+  pid_hijo1=fork();
+
+  if(pid_hijo1==0){
+
+
+
+    printf("Soy el hijo mayor , Mi padre es= %d, Mi PID= %d\n",getppid(),getpid());
+
+    sleep(4);
+
+    exit(0);
+
+  }
+  //hijo 2
+
+  pid_hijo2=fork();
+
+  if(pid_hijo2==0){
+
+    printf("Soy el hijo de enmedio, Mi padre es= %d, Mi PID= %d\n",getppid(),getpid());
+
+    sleep(2);
+
+    exit(0);
+
+  }
+  
+  //hijo 3
+
+  pid_hijo3=fork();
+
+  if(pid_hijo3==0){
+
+    printf("Soy el hijo 3, Mi padre es= %d, Mi PID= %d\n",getppid(),getpid());
+
+    sleep(3);
+
+    exit(0);
+
+  }
+  //proceso padre 
+
+  printf("  SOY EL  PADRE = %d\n",getppid());
+  
+
+  exit(0);
+
+}
+
+
+
+
+
+
+
+
+
 
 Para evitar complicaciones con máquinas virtuales, si lo prefieres puedes utilizar el compilador online: https://www.onlinegdb.com/online_c_compiler
 
