103e5ddaa61169ee7b55c1f10338ec4fb356eee5
franro12 <118440854+franro12@users.noreply.github.com>
2024-10-16 18:57:28 +0200

    Add files via upload

--- UA1/examen/dev_25/ua1ex1.c
+++ UA1/examen/dev_25/ua1ex1.c
@@ -1,24 +1,39 @@
-/*
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
-NOMBRE:
-FECHA:
+//Creo la función donde se muestra el PID del padre y el de los hijos.
+void hijo(int i){
+    printf("Hijo %d con PID %d y PID del padre %d\n", i, getpid(), getppid());
+}
 
-PARTE PRÁCTICA
+int main (){
+    pid_t pid;
+    int i;
 
-FR1: Haz un programa en C que genere una estructura de procesos con un PADRE y 3 HIJOS, del mismo padre se entiende - 2 puntos
-FR2: Visualiza por cada hijo su identificador (si es el hijo 1, 2 ó 3), su PID y el del padre, utilizando para ello una función definida por ti a la que llamen los procesos hijos - 2 puntos
-FR3: Justo antes de finalizar el programa PADRE, se debe imprimir por pantalla el PID del padre de todos una única vez. Debe hacerlo el programa PADRE - 2 puntos
-FR4: Implementa el control de errores - 2 puntos
-FR5: Documenta y estructura el código - 2 puntos
+    //Bucle donde se crean los 3 hijos.
+    for (i = 1; i <= 3; i++){
+            pid = fork();
 
-Para evitar complicaciones con máquinas virtuales, si lo prefieres puedes utilizar el compilador online: https://www.onlinegdb.com/online_c_compiler
+            //Si el resultado es -1 ocurre un error y no crea los procesos hijos.
+            if (pid == -1){
+                printf("No se ha podido crear el proceso hijo");
+                exit(-1);
+            }
+            //Si el resultado es 0 si crea los procesos hijos y se llama a la función definida antes para que los muestre.
+            if (pid == 0){
+                hijo(i);
+                exit(0);
+            }
+    }
 
-Notas:
+ //El proceso padre espera hasta que los procesos hijos hayan acabado y una vez acaba el programa finaliza muestra solo el PID del padre.    
+    if (pid > 0){
 
-Los comentarios (descriptivos y concisos) en el código ... siempre son bien.
-Los nombres de las variables autodescriptivos ... siempre son bien.
-Las impresión por pantalla, correctamente indentada y verticalmente espaciada ... siempre es bien.
-Los warnings del presente ... son los errores del futuro.
-El nombre del fichero .c a entregar debe ser: examen\dev_X\ua1ex1.c , es decir, el fichero ua1ex1.cdebe estar ubicado en tu carpeta dev_X\
+        for(i = 1; i<=3; i++){
+            wait(NULL);
+        }
 
-*/
+        printf("El programa padre ha finalizado, este sería su PID: %d\n", getpid());
+    }
+}
