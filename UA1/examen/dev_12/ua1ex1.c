16cbc992e364a761bf3d35f5e390d1b5968164c9
Antonio Gomez <antonio36772@gmail.com>
2024-10-16 18:57:09 +0200

    Examen U1 C

--- UA1/examen/dev_12/ua1ex1.c
+++ UA1/examen/dev_12/ua1ex1.c
@@ -1,7 +1,7 @@
 /*
 
-NOMBRE:
-FECHA:
+NOMBRE: Antonio Gomez Cuevas
+FECHA: 16/10/2024
 
 PARTE PRÁCTICA
 
@@ -22,3 +22,70 @@ Los warnings del presente ... son los errores del futuro.
 El nombre del fichero .c a entregar debe ser: examen\dev_X\ua1ex1.c , es decir, el fichero ua1ex1.cdebe estar ubicado en tu carpeta dev_X\
 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void mostrar_info_hijo(int id_hijo, pid_t pid_hijo, pid_t pid_padre) {
+    printf("Soy el hijo %d, mi PID es %d y el PID de mi padre es %d\n", id_hijo, pid_hijo, pid_padre);
+}
+
+int main() {
+    // Declaramos las variables
+    pid_t pid_hijo1, pid_hijo2, pid_hijo3;
+    pid_t pid_padre = getpid();
+
+    // Crear el primer hijo
+    pid_hijo1 = fork();
+
+    // Control de errores
+    if (pid_hijo1 == -1) {
+        perror("No se pudo crear el hijo 1");;
+        exit(-1);
+    }
+  
+    if (pid_hijo1 == 0) {
+        mostrar_info_hijo(1, getpid(), pid_padre);
+        exit(0);
+    }
+
+    // Crear el segundo hijo
+    pid_hijo2 = fork();
+
+    // Control de errores
+    if (pid_hijo2 == -1) {
+        perror("No se pudo crear el hijo 2");
+        exit(-1);
+    }
+  
+    if (pid_hijo2 == 0) {
+        mostrar_info_hijo(2, getpid(), pid_padre);
+        exit(0);
+    }
+
+    // Crear el tercer hijo
+    pid_hijo3 = fork();
+
+    // Control de errores
+    if (pid_hijo3 == -1) {
+        perror("No se pudo crear el hijo 3");
+        exit(-1);
+    }
+  
+    if (pid_hijo3 == 0) {
+        mostrar_info_hijo(3, getpid(), pid_padre);
+        exit(0);
+    }
+
+    // Esperar a que todos los hijos terminen
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
+
+    printf("Soy el padre, mi PID es %d\n", pid_padre);
+
+    return 0;
+}
