90a153038913e506b046e93ff9ccfa257cdbc3ec
Alejandro Luz Morales <alejandrosslm7@gmail.com>
2024-10-29 22:19:48 +0100

    Prueba Pull

--- UA1/examen/dev_0/ua1ex1_ejercicio2/Main.java
+++ UA1/examen/dev_0/ua1ex1_ejercicio2/Main.java
@@ -1,9 +1,8 @@
-import java.util.*;
+import java.util.Random;
 
 class Sensor implements Runnable {
     private String tipo;
 
-    // El constructor recibe el tipo de sensor y lo guarda.
     public Sensor(String tipo) {
         this.tipo = tipo;
     }
@@ -11,37 +10,41 @@ class Sensor implements Runnable {
     @Override
     public void run() {
         Random rand = new Random();
-        // Simulamos 10 lecturas.
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i <= 10; i++) {
+            int valor = tipo.equals("DATOS DE TEMPERATURA") ? rand.nextInt(41) :
+                    tipo.equals("DATOS DE HUMEDAD") ? rand.nextInt(101) :
+                            rand.nextInt(2);
+            int tiempoLectura = rand.nextInt(3) + 1;
+            System.out.println("[" + System.currentTimeMillis() + "] " + tipo + " - Lectura " + i + ": " + valor);
 
-            //Generamos un valor aleatorio apropiado.
-            if (tipo.equals("DATOS DE TEMPERATURA")) {
-            } else if (tipo.equals("DATOS DE HUMEDAD"))
-            System.out.println(tipo + " - DATO: " + valor + " - TIEMPO: " + System.currentTimeMillis());
-
+            try {
+                Thread.sleep(tiempoLectura * 1000);
+            } catch (InterruptedException e) {
+                System.out.println(tipo + " ha sido interrumpido.");
+            }
         }
+        System.out.println(tipo + " ha completado sus 10 ciclos de lectura.");
     }
 }
 
 public class Main {
+    public static void main(String[] args) {
+        Thread sensorTemperatura = new Thread(new Sensor("DATOS DE TEMPERATURA"));
+        Thread sensorHumedad = new Thread(new Sensor("DATOS DE HUMEDAD"));
+        Thread sensorEstadoPlantas = new Thread(new Sensor("Estado Plantas"));
 
-    public static void main(String[] args) throws InterruptedException {
-        // Creamos un hilo para cada sensor .
-        Thread dato_temperatura = new Thread(new Sensor("DATOS DE TEMPERATURA"));
-        Thread dato_humedad = new Thread(new Sensor("DATOS DE HUMEDAD"));
-        Thread dato_plantas = new Thread(new Sensor("Estado Plantas"));
+        sensorTemperatura.start();
+        sensorHumedad.start();
+        sensorEstadoPlantas.start();
 
-        // Arrancamos cada hilo.
-        dato_temperatura.start();
-        dato_humedad.start();
-        dato_plantas.start();
+        try {
+            sensorTemperatura.join();
+            sensorHumedad.join();
+            sensorEstadoPlantas.join();
+        } catch (InterruptedException e) {
+            System.out.println("La ejecución fue interrumpida.");
+        }
 
-        // Esperamos a terminen su ejecución.
-        dato_temperatura.join();
-        dato_humedad.join();
-        dato_plantas.join();
-
-        // Una vez que todos terminaran muestro un mensaje final.
-        System.out.println("DATOS COGIDOS CORRECTAMENTE.");
+        System.out.println("Monitoreo del huerto completado.");
     }
 }
