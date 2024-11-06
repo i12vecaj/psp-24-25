abdbf937142b1a442573fac0ac5c7f1db8d24e6d
albertomarbells <albertomarbells@gmail.com>
2024-11-06 18:52:15 +0100

    Cambios

--- UA2/Ejemplos_Clase/src/com/ceslopedevega/hilos/PrimerHiloR.java
+++
@@ -1,8 +0,0 @@
-package com.ceslopedevega.hilos;
-
-public class PrimerHiloR implements Runnable {
-	public void run() {
-		System.out.println("Hola desde el Hilo! " +
-	       Thread.currentThread().getId());
-	}
-}
---
+++ UA2/tareas/dev_16/tarea_1/ua2tarea1fr2.java
@@ -0,0 +1,5 @@
+package UA2.tareas.dev_16.tarea_1;
+
+public class ua2tarea1fr2 {
+    
+}
