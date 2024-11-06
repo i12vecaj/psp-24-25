994e7ac06cc11f021e6bcde80e07dc46688c81cc
Pakitoh0 <162631375+Pakitoh0@users.noreply.github.com>
2024-11-06 19:47:02 +0100

    Update ua2tarea1fr2.java

--- UA2/tareas/dev_0/tarea_1/ua2tarea1fr2.java
+++ UA2/tareas/dev_0/tarea_1/ua2tarea1fr2.java
@@ -1,5 +1,41 @@
-package UA2.tareas.dev_0.tarea_1;
-
 public class ua2tarea1fr2 {
 
+    // Variable compartida entre los hilos
+    private static int contador = 0;
+
+//este metodo nos permite incrementar usando el synchronized 
+    public synchronized static void incrementarContador() {
+        contador += 1000;
+    }
+
+    public static void main(String[] args) {
+        
+        Runnable tarea = new Runnable() {
+            @Override
+            public void run() {
+                incrementarContador();
+            }
+        };
+
+        //5hilos 
+        Thread[] hil0 = new Thread[5];
+
+        try {
+            for (int i = 0; i < 5; i++) {
+                hil0[i] = new Thread(tarea);
+                hil0[i].start();  
+            }
+            
+            for (int i = 0; i < 5; i++) {
+                hil0[i].join();
+            }
+
+            
+            System.out.println("Valor final del contador: " + contador);
+
+        } catch (InterruptedException e) {
+            // Control básico de errores
+            System.err.println("Error al ejecutar los hilos: " + e.getMessage());
+        }
+    }
 }
