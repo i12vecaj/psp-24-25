e01bc42abeaa29bcca817641f9e99cc6df21d7fc
elmatas24 <121408582+elmatas24@users.noreply.github.com>
2024-11-13 03:13:20 +0100

    Update ua2tarea1fr2.java

--- UA2/tareas/dev_18/tarea_1/ua2tarea1fr2.java
+++ UA2/tareas/dev_18/tarea_1/ua2tarea1fr2.java
@@ -1,5 +1,32 @@
-package UA2.tareas.dev_0.tarea_1;
-
 public class ua2tarea1fr2 {
-
+  //con la sincronizacion conseguimos un orden el cual sin el synchronized no teniamos 
+    private static int contador = 0;
+    public static void main(String[] args) {
+        Thread hilo1 = new Thread(() -> incrementar("hilo1"));
+        Thread hilo2 = new Thread(() -> incrementar("hilo2"));
+        Thread hilo3 = new Thread(() -> incrementar("hilo3"));
+        Thread hilo4 = new Thread(() -> incrementar("hilo4"));
+        Thread hilo5 = new Thread(() -> incrementar("hilo5"));
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+        hilo4.start();
+        hilo5.start();
+        try {
+            hilo1.join();
+            hilo2.join();
+            hilo3.join();
+            hilo4.join();
+            hilo5.join();
+        } catch (InterruptedException e) {
+            System.out.println("Ha habido un error en este hilo: " + e.getMessage());
+        }
+        System.out.println("Valor final sincronizado: " + contador);
+    }
+    private static synchronized void incrementar(String nombreHilo) {
+        for (int j = 0; j < 1000; j++) {
+            contador++;
+            System.out.println("soy" + nombreHilo + ", y he conseguido una puntuacion de: " + contador);
+        }
+    }
 }
