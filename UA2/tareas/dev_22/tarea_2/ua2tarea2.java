fabe45be711ecdec02f00edcf2a3c0aa7aaaed87
Francisco José Navarro Ruiz <142453327+KatEston23@users.noreply.github.com>
2024-11-27 19:08:09 +0100

    Update ua2tarea2.java

--- UA2/tareas/dev_22/tarea_2/ua2tarea2.java
+++ UA2/tareas/dev_22/tarea_2/ua2tarea2.java
@@ -1,5 +1,16 @@
-package UA2.tareas.dev_0.tarea_2;
-
 public class ua2tarea2 {
 
+    public static void main(String[] args) {
+        CuentaCorriente cuenta = new CuentaCorriente(1000.00f);
+
+        Thread hilo1 = new Thread(new AñadirSaldo("Jose Martinez", 1000f));
+        Thread hilo2 = new Thread(new AñadirSaldo("Jose Jose", 4000f));
+        Thread hilo3 = new Thread(new AñadirSaldo("Jose Juan", 1500f));
+
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+
+
+    }
 }
