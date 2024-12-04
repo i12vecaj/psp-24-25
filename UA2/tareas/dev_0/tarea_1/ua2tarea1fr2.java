77321846a2c777fd5c4e8d1ed6862b647864bcf9
Pakitoh0 <162631375+Pakitoh0@users.noreply.github.com>
2024-12-04 10:19:18 +0100

    Update ua2tarea1fr2.java

--- UA2/tareas/dev_0/tarea_1/ua2tarea1fr2.java
+++ UA2/tareas/dev_0/tarea_1/ua2tarea1fr2.java
@@ -1,41 +1,33 @@
-public class ua2tarea1fr2 {
+public class Ua2Tarea1FR2 {
 
     // Variable compartida entre los hilos
     private static int contador = 0;
 
-//este metodo nos permite incrementar usando el synchronized 
-    public synchronized static void incrementarContador() {
+    // Método sincronizado para incrementar el contador
+    private synchronized static void incrementarContador() {
         contador += 1000;
     }
 
     public static void main(String[] args) {
-        
-        Runnable tarea = new Runnable() {
-            @Override
-            public void run() {
-                incrementarContador();
-            }
-        };
+        Runnable tarea = () -> incrementarContador();
 
-        //5hilos 
-        Thread[] hil0 = new Thread[5];
+        // Crear 5 hilos
+        Thread[] hilos = new Thread[5];
 
-        try {
-            for (int i = 0; i < 5; i++) {
-                hil0[i] = new Thread(tarea);
-                hil0[i].start();  
-            }
-            
-            for (int i = 0; i < 5; i++) {
-                hil0[i].join();
-            }
-
-            
-            System.out.println("Valor final del contador: " + contador);
-
-        } catch (InterruptedException e) {
-            // Control básico de errores
-            System.err.println("Error al ejecutar los hilos: " + e.getMessage());
+        for (int i = 0; i < 5; i++) {
+            hilos[i] = new Thread(tarea);
+            hilos[i].start();
         }
+
+        // Esperar a que todos los hilos terminen
+        for (int i = 0; i < 5; i++) {
+            try {
+                hilos[i].join();
+            } catch (InterruptedException e) {
+                System.err.println("Error al ejecutar los hilos: " + e.getMessage());
+            }
+        }
+
+        System.out.println("Valor final del contador: " + contador);
     }
 }
