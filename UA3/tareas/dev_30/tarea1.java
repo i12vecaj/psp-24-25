76b25e3a394655dfceec712a2c9c804624f2f466
ManuelTorresAlcazar <manolotorraz@gmail.com>
2025-01-13 20:36:59 +0100

    tarea 1 UA3 terminada

---
+++ UA3/tareas/dev_30/tarea1.java
@@ -0,0 +1,48 @@
+import java.net.*;
+import java.util.Scanner;
+
+public class Main {
+    URL url;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String direccion = "";
+
+
+        try{
+
+            do {
+
+                System.out.println("Dime una direccion IP o una direccion web");
+                direccion = scanner.nextLine();
+
+                URL url = new URL(direccion);
+
+                System.out.println("El protocolo que utiliza la pagina web es: "+url.getProtocol());
+                System.out.println("El host que esta utilizando es: "+url.getHost());
+
+
+
+
+            }while (!direccion.equals("localhost"));
+
+
+
+
+
+
+        }catch (Exception e){
+            if (direccion.equals("localhost"))
+
+                System.out.println("Saliendo del programa....");
+            else
+                System.out.println("Ha ocurrido un error al mostrar la información");
+        }
+
+
+
+
+
+    }
+}
+
