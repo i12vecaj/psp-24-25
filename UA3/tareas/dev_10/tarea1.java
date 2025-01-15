c477c2d25b715a4517a79a8594e21611cf8c028e
Ignacio De Loyola Díaz Jiménez <ignaciodeloyoladiazjimenez@gmail.com>
2025-01-15 12:18:48 +0100

    Tarea URL/IP

---
+++ UA3/tareas/dev_10/tarea1.java
@@ -0,0 +1,45 @@
+import java.net.URI;
+import java.net.URL;
+import java.util.Scanner;
+
+public class tarea1 {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    String urlString = "";
+
+    //  pedir URLs/IPs hasta "localhost"
+    while (!urlString.equalsIgnoreCase("localhost")) {
+      System.out.print("Introduce una URL o IP (escribe 'localhost' para salir): ");
+      urlString = scanner.nextLine();
+
+      if (urlString.equalsIgnoreCase("localhost")) {
+        System.out.println("¡Hasta la proxima!");
+        break;
+      }
+
+      try {
+        // Convertir la cadena a URI
+        URI uri = new URI(urlString);
+        // Convertir el URI a un objeto URL
+        URL url = uri.toURL();
+
+        // Mostrar información URL/IP
+        System.out.println("Información de la URL/IP");
+        System.out.println("Host: " + url.getHost());
+        System.out.println("Protocolo: " + url.getProtocol());
+
+
+        if (url.getPort() == -1) {
+          System.out.println("Puerto: (predeterminado)");
+        } else {
+          System.out.println("Puerto: " + url.getPort());
+        }
+        System.out.println("Ruta: " + url.getPath());
+      } catch (Exception error) {
+        System.out.println("error: " + error.getMessage());
+      }
+    }
+    scanner.close();
+  }
+}
