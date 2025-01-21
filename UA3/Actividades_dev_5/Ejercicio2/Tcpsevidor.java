7c6b37b9dbc410475c2cba584d380d4a737ea6e0
Pakitoh0 <162631375+Pakitoh0@users.noreply.github.com>
2025-01-21 22:38:08 +0100

    Update Tcpsevidor.java

--- UA3/Actividades_dev_5/Ejercicio2/Tcpsevidor.java
+++ UA3/Actividades_dev_5/Ejercicio2/Tcpsevidor.java
@@ -1,1 +1,31 @@
+import java.io.*;
+import java.net.*;
+
+public class Tcpsevidor {
+    public static void main(String[] args) throws IOException {
+        int puerto = 6000; // Puerto
+        ServerSocket servidor = new ServerSocket(puerto);
+        System.out.println("Servidor escuchando en el puerto " + servidor.getLocalPort());
+
+        // Acepta conexión de cliente 1
+        System.out.println("Esperando cliente 1...");
+        Socket cliente1 = servidor.accept();
+        System.out.println("Cliente 1 conectado:");
+        System.out.println("Puerto Local: " + cliente1.getLocalPort());
+        System.out.println("Puerto Remoto: " + cliente1.getPort());
+
+        // Acepta conexión de cliente 2
+        System.out.println("Esperando cliente 2...");
+        Socket cliente2 = servidor.accept();
+        System.out.println("Cliente 2 conectado:");
+        System.out.println("Puerto Local: " + cliente2.getLocalPort());
+        System.out.println("Puerto Remoto: " + cliente2.getPort());
+
+        // Cerrar sockets
+        cliente1.close();
+        cliente2.close();
+        servidor.close();
+        System.out.println("Conexiones cerradas.");
+    }
+}
 
