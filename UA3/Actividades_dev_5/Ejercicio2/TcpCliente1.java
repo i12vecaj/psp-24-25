1ef8d839af12b0f9c42d2a2837381538da0c578a
Pakitoh0 <162631375+Pakitoh0@users.noreply.github.com>
2025-01-21 22:38:45 +0100

    Update and rename TcpCliente.java to TcpCliente1.java

---
+++ UA3/Actividades_dev_5/Ejercicio2/TcpCliente1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+public class TcpCliente1{
+    public static void main(String[] args) throws IOException {
+        String host = "localhost";
+        int puerto = 6000; // Puerto
+
+        Socket cliente = new Socket(host, puerto);
+        InetAddress i = cliente.getInetAddress();
+        System.out.println("Cliente 1 conectado:");
+        System.out.println("Puerto Local: " + cliente.getLocalPort());
+        System.out.println("Puerto Remoto: " + cliente.getPort());
+        System.out.println("Dirección IP Host Remoto: " + i.getHostAddress());
+        System.out.println("Nombre Host/IP Remoto: " + i.getHostName());
+
+        cliente.close();
+    }
+}
+
