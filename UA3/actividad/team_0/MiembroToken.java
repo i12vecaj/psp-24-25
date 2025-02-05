d5263276a7544030a5791c2e8a3e37e51c16d4a6
Alexdro2804 <128082678+Alexdro2804@users.noreply.github.com>
2025-02-05 13:39:42 +0100

    Update MiembroToken.java

--- UA3/actividad/team_0/MiembroToken.java
+++ UA3/actividad/team_0/MiembroToken.java
@@ -1,37 +1,99 @@
-/**
- * @file        MiembroToken.java
- *
- * @brief       Solución para la actividad evaluable de la UA3.
- * @date        14/12/2022
- * @author      Nombre Apellidos Miembro 1 - github@email (dev_A)
- * @author      Nombre Apellidos Miembro 2 - github@email (dev_B)
- * @author      Nombre Apellidos Miembro 3 - github@email (dev_C)
- * @author      Nombre Apellidos Miembro 4 - github@email (dev_D)
- *
- * @note        ¡Pero qué diablos!
- * @note        https://docs.google.com/presentation/d/e/2PACX-1vQzxbSl2IXLxelLggksAWEQd2tDT-5sNqETQCpIHCnNBqjoSmbvlJdCcOfu_rjQLz_BN6lsoOjimqSO/pub?start=false&loop=false&delayms=3000
- */
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.Duration;
 
-/*
-* @class    MiembroToken
-* @brief    Clase para simular el comportamiento de una red token ring
-*
-* @todo FR1 [5 puntos]: Implementa la clase MiembroToken con la funcionalidad descrita previamente. Para comprobar la correcta ejecución del sistema, la clase MiembroToken debe imprimir por pantalla la suficiente información para ver el estado de cada Miembro de la red.
-* @todo FR2 [2,5 puntos]: Mejora la clase MiembroToken para poder ejecutar su funcionalidad, en este caso, dormir una cantidad de tiempo determinada, como un hilo.
-* @todo FR4 [1 punto]: Mejora la clase MiembroToken para crear una red token ring de anillo doble, es decir, se puede tener otro token en otro anillo virtual, en sentido contrario.
-*/
+public class MiembroToken implements Runnable {
+    private int id;
+    private int puerto;
+    private boolean tieneToken;
+    private boolean esUltimo;
+    private int puertoSiguiente;
+    private long tiempoEspera;
+    private LocalDateTime ultimaRecepcion;
 
-public class MiembroToken
-{
-    /* @brief       Classic java main, starting execution
-     * @param       arg[0] = [id]
-     * @param       arg[1] = [puerto]
-     * @param       arg[2] = [token_al_inicio]
-     * @param       arg[3] = [soy_el_ultimo]
-     */
-    public static void main(String[] args)
-    {
+    public MiembroToken(int id, int puerto, boolean tieneToken, boolean esUltimo) {
+        this.id = id;
+        this.puerto = puerto;
+        this.tieneToken = tieneToken;
+        this.esUltimo = esUltimo;
+        this.puertoSiguiente = esUltimo ? 10000 : puerto + 1;
+        this.tiempoEspera = id * 1000;
+        this.ultimaRecepcion = LocalDateTime.now();
+    }
 
-    } // End of main()
+    @Override
+    public void run() {
+        try (DatagramSocket socket = new DatagramSocket(puerto)) {
+            System.out.println("Miembro " + id + " escuchando en el puerto " + puerto);
 
-} // End of class
+            if (tieneToken) {
+                procesarToken();
+            }
+
+            byte[] buffer = new byte[256];
+            while (true) {
+                DatagramPacket paquete = new DatagramPacket(buffer, buffer.length);
+                socket.receive(paquete);
+
+                String mensaje = new String(paquete.getData(), 0, paquete.getLength(), StandardCharsets.UTF_8);
+                if (mensaje.equals("TRUE")) {
+                    procesarToken();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void procesarToken() {
+        LocalDateTime ahora = LocalDateTime.now();
+        if (ultimaRecepcion != null) {
+            Duration duracion = Duration.between(ultimaRecepcion, ahora);
+            System.out.println("Miembro " + id + " recibió el TOKEN. Tiempo desde la última recepción: " + duracion.toMillis() + " ms");
+        }
+        ultimaRecepcion = ahora;
+
+        try {
+            System.out.println("Miembro " + id + " está procesando durante " + (tiempoEspera / 1000) + " segundos.");
+            Thread.sleep(tiempoEspera);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        enviarToken();
+    }
+
+    private void enviarToken() {
+        try (DatagramSocket socket = new DatagramSocket()) {
+            InetAddress direccion = InetAddress.getByName("localhost");
+            byte[] buffer = "TRUE".getBytes(StandardCharsets.UTF_8);
+            DatagramPacket paquete = new DatagramPacket(buffer, buffer.length, direccion, puertoSiguiente);
+
+            socket.send(paquete);
+            System.out.println("Miembro " + id + " envió el TOKEN al puerto " + puertoSiguiente);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            System.out.println
+                    ("Uso: java MiembroToken [id] [puerto] [token_al_inicio] [soy_el_ultimo]");
+            return;
+        }
+
+        int id = Integer.parseInt(args[0]);
+        int puerto = Integer.parseInt(args[1]);
+        boolean tieneToken = args[2].equalsIgnoreCase("yes");
+        boolean esUltimo = args[3].equalsIgnoreCase("yes");
+
+        MiembroToken miembro = new MiembroToken(id, puerto, tieneToken, esUltimo);
+        new Thread(miembro).start();
+    }
+}
