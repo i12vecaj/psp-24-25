92270d6c4201fb18f794ea06dd5312f45685b77f
Adrian Cruz Barranco <adriancb2023@gmail.com>
2025-03-05 17:44:35 +0100

    entrega examen

---
+++ UA4/examen/Person.java
@@ -0,0 +1,26 @@
+package JuanitoTornillero;
+
+public class Person {
+    private String name;
+    private String about;
+    private int birthYear;
+    private String password;
+
+    public Person(String name, String about, int birthYear) {
+        this.name = name;
+        this.about = about;
+        this.birthYear = birthYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+}
