6475895d938a4b9773085a37fc6fc575765dc5e9
Adrian Cruz Barranco <adriancb2023@gmail.com>
2025-03-10 15:19:42 +0100

    entrega tarea lunes 10

---
+++ UA5/tareas/dev_8/SimpleCallbackHandler.java
@@ -0,0 +1,25 @@
+package adriancruz.conceptos;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import java.io.IOException;
+
+class SimpleCallbackHandler implements CallbackHandler {
+    @Override
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        for (Callback callback : callbacks) {
+            if (callback instanceof NameCallback) {
+                NameCallback nameCallback = (NameCallback) callback;
+                nameCallback.setName("user"); // Set the username
+            } else if (callback instanceof PasswordCallback) {
+                PasswordCallback passwordCallback = (PasswordCallback) callback;
+                passwordCallback.setPassword("password".toCharArray()); // Set the password
+            } else {
+                throw new UnsupportedCallbackException(callback, "Unrecognized Callback");
+            }
+        }
+    }
+}
